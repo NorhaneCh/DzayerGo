54202a0a7a54dfdc24cae00d5a27a5f84d375798
NorhaneCh <kn_cherif@esi.dz>
2023-06-27 08:45:27 +0100

    update done

--- app/src/App.js
+++ app/src/App.js
@@ -1,16 +1,13 @@
-import Carousel from "react-multi-carousel";
-import "react-multi-carousel/lib/styles.css";
-
+import { Link } from "react-router-dom";
+import { useEffect } from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 import logo_white from "./images/logo_white.png";
-import appstore from "./images/appstore.png";
-import home from "./images/home.png";
+import logo from "./images/logo.png";
 import notre_dame from './images/notre_dame.png';
-import googleplay from "./images/googleplay.png";
-import vect1 from "./images/vect1.png";
 import line from "./images/Line.png";
-import visiter from './images/visiter.png';
 import solvinno from './images/solvinno.png';
-import connecter from './images/connecter.png';
 import black_line from './images/black_line.png';
 import pink_line from './images/pink_line.png';
 import frame1 from "./images/frame1.png";
@@ -19,165 +16,197 @@ import frame3 from "./images/frame3.png";
 import frame4 from "./images/frame4.png";
 import frame5 from "./images/frame5.png";
 import frame6 from "./images/frame6.png";
-import button from "./images/button.png";
-import map from "./images/ligne.png";
+import location from "./images/location.png";
+import phone from "./images/phone.png";
+import email from './images/email.png';
 import "./App.css";
+import Aos from "aos";
+import'aos/dist/aos.css';
+import ButtonMailto from "./components/ButtonMailto.js";
 
 function App() {
-  const responsive = {
-    superLargeDesktop: {
-      // the naming can be any, depends on you.
-      breakpoint: { max: 4000, min: 3000 },
-      items: 5,
-    },
-    desktop: {
-      breakpoint: { max: 3000, min: 1024 },
-      items: 3,
-    },
-    tablet: {
-      breakpoint: { max: 1024, min: 464 },
-      items: 2,
-    },
-    mobile: {
-      breakpoint: { max: 464, min: 0 },
-      items: 1,
-    },
+
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, []);
+
+  var settings = {
+    dots:true,
+    infinite: true,
+    speed:500,
+    slidesToShow:4,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          initialSlide: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   };
+  
   return (
     <div className=" pt-0 align-center">
       <div id="hero" className="bg relative">
-        <div className="flex flex-row gap-[700px]">
+        <div className="flex flex-row gap-[1000px]">
           <img className="pl-16 mt-9 w-[166px]" src={logo_white} alt="" />
-          <div className="flex flex-row gap-52">
-          <div className="pt-9 flex flex-row gap-28 justify-center ">
-
-            <a href="#about us">About us</a>
-            <a href="#download">Download</a>
-           
-          </div>
-          <div>
-          <a href="#footer"><img className="pt-9" src={connecter} alt="" /></a>
-          </div>
-          </div>
+          
+          <nav className='mx-9 mt-12'>
+            <a className=' p-[10px] ml-[20px] ' href="#about us"> About us </a> 
+            <a className='p-[10px] ml-[20px] ' href="#download"> Download </a>
+          </nav>
+        
         </div>
+
         <div className="flex flex-col mt-16 justify-center items-center">
-          <p className="text-[60px] font-bold  text-dzayergo-white">
+          <p className="text-[45px] font-bold  text-dzayergo-white">
             Discover the magic of
           </p>
-          <p className="text-[60px] font-bold text-dzayergo-pink">Algeria</p>
-          <a href="#download">
-            <img className="mt-[200px]" src={button} alt="DzayerGo" />
-          </a>
+          <p className="text-[45px] font-bold text-dzayergo-pink">Algeria</p>
+           <a className="btn_go" href="start">GO</a>
+          
         </div>
-        <div className="absolute bottom-28 right-[200px] rotate-12">
-          <img src={frame1} alt="" />
+        <div className="absolute bottom-28 z-10 right-[150px] rotate-[20deg]">
+          <img className="h-[400px]" src={frame1} alt="" />
         </div>
-        <div>
-          <img className="absolute bottom-11/12 w-[1285px]" src={line} alt="line" />
+        <div className="absolute w-[1260px] bottom-28 z-1 ">
+          <img className="rotate-180" src={line} alt="line" />
         </div>
       </div>
-      <div   id="about us" className=" relative flex flex-row gap-24 mt-36 justify-center  ">
-        <div className="absolute top-40 mr-[-1700px] ">
-        <img src={black_line} alt="" />
+      <div id="start" className=" relative flex flex-row gap-36 mt-36 justify-center">
+        <div data-aos="fade-left" className="absolute top-40 ml-[1260px]">
+        <img src={black_line} alt="line" />
         </div>
-        <img className="w-[728px] h-[649px]" src={notre_dame} alt="Notre dame d'afrique" />
+        
+        <img data-aos="fade-right" className="w-[600px] h-[400px]" src={notre_dame} alt="Notre dame d'afrique" />
         <div>
-          <span className="text-dzayergo-pink text-[48px] font-bold">DzayerGO</span><span className="text-[48px] font-normal">, la meilleure application</span>
-          <p className="text-[48px] font-normal">pour découvrir l’Algérie</p>
-          <p className="text-[30px] w-[80%] mt-16 text-justify">Notre application sans égale ,offre des fonctionnalités quasiment ou rarement vues sur d’autres exemplaires d’applications. Que vous voudriez visiter virtuellement ou réellement n’importe quel lieu de votre choix, maintenant ceci vous est possible!
-          Une bonne nouvelle! notre application est destinée sans exception à tout le monde ,disponible en 3 langues. 
+          <span className="text-dzayergo-pink text-[40px] font-bold">DzayerGO</span>
+          <span className="text-[40px] font-normal">, The best application</span>
+          <p className="text-[40px] font-normal">to discover Algeria</p>
+          <p className="text-[25px] w-[90%] mt-16 text-justify">Our unparalleled application offers features that are almost or rarely seen in other application instances. Whether you want to virtually or actually visit any place of your choice, now this is possible for you!
+          Great news! Our application is intended for everyone without exception, available in 3 languages.
         </p>
         </div>
       </div>
-      <div className="flex flex-row gap-0 mt-36 justify-center items-center ">
-      <div className="ml-28 w-[60%]">
-          <p className="text-[48px] font-normal">Qui sommes nous? L’équipe </p>
-          <p className="text-[48px] font-bold text-dzayergo-green">Solvinno !</p>
-          <p className="text-[30px] w-[80%] mt-12 text-justify">Spécialisée dans les services numériques, depuis son apparition en 2012 ,nous engageons à offrir des solutions innovantes et de qualité à now clients. Avec une équipe talentueuse, dotée de compétences  incontestables, nous sommes en mesure de relever des défis les plus complexes. 
- 
+      <div id="about us" className="flex flex-row  mt-36 justify-center items-center ">
+      <div className="w-[70%] pl-20">
+          <p className="text-[40px] font-normal">Who are we? The team </p>
+          <p className="text-[40px] font-bold text-dzayergo-green">Solvinno !</p>
+          <p className="text-[25px] w-[90%] mt-12 text-justify">Specialized in digital services since its appearance in 2012, we are committed to offering innovative and high-quality solutions to our clients. With a talented team equipped with undeniable skills, we are capable of tackling the most complex challenges.
         </p>
         </div>
-        <div className="flex flex-col justify-center items-center gap-6">
-        <img className="w-[826px] h-[619px]" src={solvinno} alt="solvinno" />
-        <img src={visiter} alt="" />
+        <div className="flex flex-col   items-center gap-6">
+        <img data-aos="fade-left" className="w-[600px] h-[400px]" src={solvinno} alt="solvinno" />
+        <button data-aos="fade-left" className="btn1">Visit our website</button>
         </div>
       </div>
       <div id="download" className=" relative mt-28">
-        <div className="absolute top-40 ml-[85%]">
+        <div data-aos="fade-left"className="absolute top-40 ml-[80%]">
         <img src={pink_line} alt="" />
         </div>
-        <div className="absolute bottom-[-70px]">
-          <img className="rotate-[180deg]" src={pink_line} alt="" />
+        <div data-aos="fade-right" className="absolute bottom-[-70px]">
+          <img className="rotate-180" src={pink_line} alt="" />
         </div>
-        <p className="text-dzayergo-black text-[48px] text-center font-medium mt-9">
-        Télécharger Notre Application Mobile!
+        <p className="text-dzayergo-black text-[40px] text-center font-medium mt-9">
+        Download our mobile application
         </p>
-        <p className="text-dzayergo-pink text-[52px] text-center font-medium ">
+        <p className="text-dzayergo-pink text-[40px] text-center font-bold ">
         DzayerGO
         </p>
-        <div className="slider">
-          <Carousel responsive={responsive}>
-            <div>
-              <img className="w-[292] h-[582px]" src={frame1} alt="" />
-            </div>
-            <div>
-              <img className="w-[292] h-[582px]" src={frame2} alt="" />
-            </div>
-            <div>
-              <img className="w-[292] h-[582px]" src={frame3} alt="" />
-            </div>
-            <div>
-              <img className="w-[292] h-[582px]" src={frame4} alt="" />
-            </div>
-            <div>
-              <img className="w-[292] h-[582px]" src={frame5} alt="" />
-            </div>
-            <div>
-              <img className="w-[292] h-[582px]" src={frame6} alt="" />
-            </div>
-          </Carousel>
-          ;
-        </div>
 
-      </div>
+        <Slider className="slider" {...settings}>
+        <div>
+              <img className="h-[500px]" src={frame1} alt="" />
+            </div>
+            <div>
+              <img className="h-[500px]" src={frame2} alt="" />
+            </div>
+            <div>
+              <img className="h-[500px]" src={frame3} alt="" />
+            </div>
+            <div>
+              <img className="h-[500px]" src={frame4} alt="" />
+            </div>
+            <div>
+              <img className="h-[500px]" src={frame5} alt="" />
+            </div>
+            <div>
+              <img className="h-[500px]" src={frame6} alt="" />
+            </div>
+  </Slider>
+
+  </div>
       <div
-        id="download"
-        className="relative bg-dzayergo-purple h-[329px] pt-6 mt-36"
+        className="relative bg-dzayergo-purple h-[200px] pt-6 mt-36"
       >
-        <div className="absolute top-[-60px]">
-          <img className="rotate-[-7deg] w-[1920px]" src={vect1} alt="" />
-        </div>
-        <div className="flex flex-col lg:flex-row xl:flex-row justify-center items-center gap-32 mt-16">
-          <a href="#">
-            <img className="w-[378px] h-[94px]" src={appstore} alt="" />
-          </a>
-          <a href="#">
-            <img className="w-[378px] h-[94px]" src={googleplay} alt="" />
-          </a>
+
+        <div className="flex flex-col lg:flex-row justify-center items-center gap-32 mt-12">
+        {/*-- App Store button */}
+        <a href="https://www.apple.com/fr/app-store/" target="_blank" class="market-btn apple-btn" role="button">
+       <span class="market-button-subtitle">Download on the</span>
+       <span class="market-button-title">App Store</span>
+       </a>
+       {/*-- Google Play button */}
+       <a href="https://play.google.com/store/apps?hl=fr&gl=US&pli=1" target="_blank" class="market-btn google-btn" role="button">
+      <span class="market-button-subtitle">Download on the</span>
+      <span class="market-button-title">Google Play</span>
+      </a>
+
         </div>
       </div>
 
-      <div id="footer" className=" footer bg-black h-[100px]">
-        <div className="flex flex-row justify-center items-center gap-72 mt-10">
-          <div className=" flex flex-row gap-6 justify-center ">
-            <a className="text-dzayergo-black" href="#">
-              {" "}
-              | Privacy Policy{" "}
-            </a>
-            <a className="text-dzayergo-black" href="#">
-              | Terms of Use
-            </a>
-            <a className="text-dzayergo-black" href="#">
-              | Marketing Policy
-            </a>
-            <a className="text-dzayergo-black" href="#">
-              | Help
-            </a>
+      <div id="footer" className="footer mb-12">
+        <div className="flex lx:flex-row justify-center mt-10 gap-20">
+          <div>
+            <img className="w-[166px]" src={logo}/>
           </div>
-          <p className="text-[24px]">Contact us : DzayerGO@gmail.com</p>
+          <div className="flex flex-col gap-20 items-center">
+          <div className="flex flex-row gap-6 justify-center">
+            <a href="#">| Privacy Policy</a>
+            <a href="#">| Terms of Use</a>
+            <a href="#">| Marketing Policy</a>
+            <a href="#">| Help</a>
+          </div>
+          <p className="text-xl">DzayerGO@2023 All rights Reserved </p>
+          </div>
+          <div className="flex flex-col gap-6">
+            <div className="flex flex-row gap-6">
+              <img src={location}/>
+              <Link to="https://goo.gl/maps/VFiGzHnZwLM5dp3K8"><p>Rue 24, Oued Smar</p></Link>
+            </div>
+            <div className="flex flex-row gap-6">
+              <img src={phone}/>
+              <a href="tel:+213-34453466"><p>+213-34453466</p></a>
+            </div>
+            <div className="flex flex-row gap-6">
+              <img src={email}/>
+              <ButtonMailto label="DzayerGO@gmail.com" mailto="mailto:dzayergo@gmail.com" />
+            </div>
+          </div>
+          
         </div>
       </div>
-    </div>
+    </div>  
   );
 }
 
